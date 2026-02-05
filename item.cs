0ca1733feab8d3a58846ad1144f607bc2477b4f6
Njabulo Matshika <matshikanjabulo@gmail.com>
2026-02-05 13:11:30 +0000

    using System;
    namespace myInventorySystem
    {
        public enum ItemType{Weapon, Armor, Potion, Material, Quest}
    
        public class Item
        {
            //Properties
            public int Id { get; }
            public string Name { get; set; }
            public ItemType Type { get; set; }
            public int Value { get; set; }
            public float Weight { get; set; }
            public string Description { get; set; }
    
            //Constructor
            public Item(int id, string name, ItemType type, int value, float weight)
            {
                Id = id;
                Name = name;
                Type = type;
                Value = value;
                Weight = weight;
                Description = string.Empty;
            }
    
            //Methods
            public override string ToString()
            {
                return $"{Name} ({Type}) - Value: {Value}g, Weight: {Weight}kg";
            }
    
            public string GetDetailedInfo()
            {
                return $"ID: {Id}\nName: {Name}\nType: {Type}\nValue: {Type}\nValue: {Value}g\nWeight: {Weight}kg\nDescription: {Description}";
            }
        }
    }

---
+++ item.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace myInventorySystem
+{
+    public enum ItemType{Weapon, Armor, Potion, Material, Quest}
+
+    public class Item
+    {
+        //Properties
+        public int Id { get; }
+        public string Name { get; set; }
+        public ItemType Type { get; set; }
+        public int Value { get; set; }
+        public float Weight { get; set; }
+        public string Description { get; set; }
+
+        //Constructor
+        public Item(int id, string name, ItemType type, int value, float weight)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Value = value;
+            Weight = weight;
+            Description = string.Empty;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"{Name} ({Type}) - Value: {Value}g, Weight: {Weight}kg";
+        }
+
+        public string GetDetailedInfo()
+        {
+            return $"ID: {Id}\nName: {Name}\nType: {Type}\nValue: {Type}\nValue: {Value}g\nWeight: {Weight}kg\nDescription: {Description}";
+        }
+    }
+}
+
