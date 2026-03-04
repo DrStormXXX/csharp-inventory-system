fdc7153dee5916829ccceb081ce07262b8d81fd1
Njabulo Matshika <matshikanjabulo@gmail.com>
2026-03-04 15:19:45 +0000

    using System;
    namespace myConsoleHelper
    {
        public static class ConsoleHelper
        {
            public static void PrintSuccess(string message)
            {
                Console.ForegroundColor = ConsoleColor.Green;
                Console.WriteLine($"Success: {message}");
                Console.ResetColor();
            }
    
        }
    }

--- ConsoleHelper.cs
+++ ConsoleHelper.cs
@@ -2,4 +2,15 @@ using System;
 
 namespace myConsoleHelper
 {
+    public static class ConsoleHelper
+    {
+        public static void PrintSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Success: {message}");
+            Console.ResetColor();
+        }
+        
+        
+    }
 }
